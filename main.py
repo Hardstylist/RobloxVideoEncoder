989786dd1458ce4ed5c3bc959424cc17354b398c
Luhkaaa <97032624+Hardstylist@users.noreply.github.com>
2022-02-03 21:24:21 +0000

    aertaer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("ExampleVideo","ExampleMovie1.mp4",.2,.17,20)
+    encoder.encode("ExampleVideo","ExampleMovie1.mp4",.5,.20,30)
 
 if __name__== "__main__":
     main()
